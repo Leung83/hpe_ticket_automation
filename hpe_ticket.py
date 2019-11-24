0c8492731fc6dc89e6a9fb3101524b99e811d7eb
Leung83 <31999309+Leung83@users.noreply.github.com>
2019-11-24 11:16:28 -0600

    v1
    # INSTRUCTIONS
    # 1. Install Python3 for windows 
    # 2. Download webdriver for you Chrome version Help > About Chrome
    # https://sites.google.com/a/chromium.org/chromedriver/downloads
    # 3. Unzip and place the chromedriver in the same folder as the script
    # 4. Install Selenium "pip install selenium"
    # 5. Update input.txt with you HPE credentials

---
+++ hpe_ticket.py
@@ -0,0 +1,63 @@
+#! /usr/bin/python3
+
+# INSTRUCTIONS
+# 1. Install Python3 for windows 
+# 2. Download webdriver for you Chrome version Help > About Chrome
+# https://sites.google.com/a/chromium.org/chromedriver/downloads
+# 3. Unzip and place the chromedriver in the same folder as the script
+# 4. Install Selenium "pip install selenium"
+# 5. Update input.txt with you HPE credentials
+
+import time  
+from selenium import webdriver      
+from selenium.webdriver.common.keys import Keys         # Module to send keystrokes
+from selenium.webdriver.support.select import Select    # Module to select drop down
+
+# Load input_data.txt
+f = open("input.txt", "r")
+user = f.readline().strip()
+password = f.readline().strip()
+said = f.readline().strip()
+f.close()
+ 
+# Creating an instance webdriver 
+driver = webdriver.Chrome()  
+# Maximizing window
+driver.maximize_window()
+driver.get('https://support.hpe.com/portal/site/hpsc/scm/home') 
+# Wait for page to load
+time.sleep(2)
+
+# Login
+#===================================
+driver.find_element_by_id('username').send_keys(user)
+driver.find_element_by_id('password').send_keys(password)
+driver.find_element_by_id('signIn').click()
+time.sleep(10)
+
+# Find all ids on page
+#ids = driver.find_elements_by_xpath('//*[@id]')
+#for i in ids:
+#    # print i.tag_name
+#    print(i.get_attribute('id'))
+
+# Find the dynamic generated iframe id
+iframeid = driver.find_element_by_tag_name('iframe')
+driver.switch_to.frame(iframeid.get_attribute('id'))
+# Provide SAID, need to switch to active iframe to find elements
+driver.find_element_by_id('ctl00_ctl00_ContentPlaceHolder1_ContentPlaceHolder3Col_txtSubmitCase').send_keys(said)
+driver.find_element_by_id('ctl00_ctl00_ContentPlaceHolder1_ContentPlaceHolder3Col_btnSubmitCase').click()  
+time.sleep(15)
+
+# Fill in case details defaults
+#===================================
+# Find the dynamic generated iframe id
+iframeid = driver.find_element_by_tag_name('iframe')
+driver.switch_to.frame(iframeid.get_attribute('id'))
+driver.find_element_by_id('environmentsList').send_keys("Red Hat Enterprise Linux")
+driver.find_element_by_id('ctl00_ctl00_ContentPlaceHolder1_ContentPlaceHolder3Col_txtContactHoursORTimezone').send_keys('8am-5pm CT')
+
+
+
+
+
